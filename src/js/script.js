cd723034ceacc247f75cde4443077690740965be
Vladislav Surin <vlad_surin@mail.ru>
2023-06-03 20:51:40 +0300

    Implemented sending emails. Added .editorconfig

--- src/js/script.js
+++ src/js/script.js
@@ -1,94 +1,126 @@
-'use strict';
+"use strict";
 
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1000,
+    draggable: false,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="./icons/left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="./icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        draggable: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("ul.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
     });
+  }
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-            .addClass('catalog__tab_active').siblings()
-            .removeClass('catalog__tab_active').closest('div.container')
-            .find('ul.catalog__content').removeClass('catalog__content_active')
-            .eq($(this).index()).addClass('catalog__content_active');
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
     });
-    
+  });
 
-    function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-        })
+  // Validation
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!"),
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно. введен адрес почты",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation form");
+  validateForms("#order form");
+  validateForms("#consultation-form");
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
     }
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+    $.ajax({
+      type: "POST",
+      url: "./mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+      console.log("dfgdfg");
 
-    // Modal
-
-    $('[data-modal=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn('slow');
+      $("form").trigger("reset");
     });
-
-    $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    });
-
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
-        })
-    });
-
-    // Validation
-
-    function validateForms(form) {
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minLength: 2
-                },
-                phone: 'required',
-                email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                name: {
-                    required: "Пожалуйста, введите свое имя",
-                    minLength: jQuery.validator.format("Введите {0} символа!")
-                },
-                phone: "Пожалуйста, введите свой номер телефона",
-                email: {
-                    required: "Пожалуйста, введите свою почту",
-                    email: "Неправильно. введен адрес почты"
-                }
-            }
-        });
-    }
-
-    validateForms('#consultation form');
-    validateForms('#order form');
-    validateForms('#consultation-form');
-
-    $('input[name=phone]').mask('+7 (999) 999-99-99');
+    return false;
+  });
 });
