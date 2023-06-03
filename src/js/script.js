53cc0b542895fcdae83342a232b0597fc338e7cb
Vladislav Surin <vlad_surin@mail.ru>
2023-06-04 00:07:28 +0300

    Added animation for reviews. Fixed validation bugs

--- src/js/script.js
+++ src/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
         $(".catalog-item__content")
           .eq(i)
           .toggleClass("catalog-item__content_active");
-        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+        $(".catalog-item__more").eq(i).toggleClass("catalog-item__more_active");
       });
     });
   }
@@ -147,4 +147,6 @@ $(document).ready(function () {
       );
     }
   });
+
+  new WOW().init();
 });
